afa70ec79269660d82ef05f073c75aec6fc70beb
AMissteryKitty <aline.nguyen@epitech.eu>
2026-03-06 14:32:27 +0100

    fix: move Action enum class to common, remove HOVER status, add comments method

--- inputs/InputHandler.cpp
+++ inputs/InputHandler.cpp
@@ -1,21 +1,19 @@
 #include "InputHandler.hpp"
+#include "types/types.hpp"
 #include "raylib.h"
 
-static const std::unordered_map<inputs::Key, inputs::Actions> commands = {
-    { {key: KEY_UP, status: inputs::Status::PRESSED }, inputs::Actions::MOVE_UP },
-    { {key: KEY_DOWN, status: inputs::Status::PRESSED }, inputs::Actions::MOVE_DOWN },
-    { {key: KEY_RIGHT, status: inputs::Status::PRESSED }, inputs::Actions::MOVE_RIGHT },
-    { {key: KEY_LEFT, status: inputs::Status::PRESSED }, inputs::Actions::MOVE_LEFT }
+static const std::unordered_map<inputs::Key, common::Action> commands = {
+    { {key: KEY_UP, status: inputs::Status::PRESSED }, common::Action::MOVE_CAMERA_UP },
+    { {key: KEY_DOWN, status: inputs::Status::PRESSED }, common::Action::MOVE_CAMERA_DOWN },
+    { {key: KEY_RIGHT, status: inputs::Status::PRESSED }, common::Action::MOVE_CAMERA_RIGHT },
+    { {key: KEY_LEFT, status: inputs::Status::PRESSED }, common::Action::MOVE_CAMERA_LEFT }
 };
 
 void inputs::InputHandler::updateKeys()
 {
     for (auto &command : commands) {
         if (IsKeyDown(command.first.key)) {
-            if (this->_keysHandler[command.first.key] == inputs::Status::PRESSED)
-                this->_keysHandler[command.first.key] = inputs::Status::HOVER;
-            else
-                this->_keysHandler[command.first.key] = inputs::Status::PRESSED;
+            this->_keysHandler[command.first.key] = inputs::Status::PRESSED;
         } else {
             if (this->_keysHandler[command.first.key] == inputs::Status::RELEASED)
                 this->_keysHandler[command.first.key] = inputs::Status::DEFAULT;
@@ -26,7 +24,7 @@ void inputs::InputHandler::updateKeys()
     return;
 }
 
-void inputs::InputHandler::getActions(std::list<inputs::Actions> &actions)
+void inputs::InputHandler::getActions(std::list<common::Action> &actions)
 {
     this->updateKeys();
     for (auto &command : commands) {
--- inputs/InputHandler.hpp
+++ inputs/InputHandler.hpp
@@ -10,7 +10,6 @@ namespace inputs {
     enum class Status {
         DEFAULT,
         PRESSED,
-        HOVER,
         RELEASED
     };
 
@@ -19,23 +18,17 @@ namespace inputs {
         Status status;
     } Key;
 
-    enum class Actions {
-        NONE,
-        MOVE_CAMERA_UP,
-        MOVE_CAMERA_DOWN,
-        MOVE_CAMERA_RIGHT,
-        MOVE_CAMERA_LEFT,
-        CHANGE_SELECTION,
-        CHANGE_SELECTION_REVERSED
-    };
-
     class InputHandler {
         public:
             InputHandler() = default;
             ~InputHandler() = default;
 
-            void getActions(std::list<inputs::Actions> &actions);
+            // getActions detects inputs and adds actions in list given in parameters.
+            void getActions(std::list<common::Action> &actions);
         private:
+            // updateKeys checks keyboards input status and updates _keysHandler.
+            void updateKeys();
+
             std::unordered_map<KeycodeRaylib, inputs::Status> _keysHandler;
     };
 }
