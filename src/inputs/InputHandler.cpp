d9a9489b539aae67b59ff0e1797616336c58073d
AMissteryKitty <aline.nguyen@epitech.eu>
2026-03-09 09:42:45 +0100

    chore: clean code

---
+++ src/inputs/InputHandler.cpp
@@ -0,0 +1,33 @@
+#include "InputHandler.hpp"
+
+static const std::unordered_map<KeyboardKey, std::unordered_map<inputs::Status, common::Action>> commands = {
+    {KEY_UP, {{inputs::Status::PRESSED, common::Action::MOVE_CAMERA_UP}}},
+    {KEY_DOWN, {{inputs::Status::PRESSED, common::Action::MOVE_CAMERA_DOWN}}},
+    {KEY_RIGHT, {{inputs::Status::PRESSED, common::Action::MOVE_CAMERA_RIGHT}}},
+    {KEY_LEFT, {{inputs::Status::PRESSED, common::Action::MOVE_CAMERA_LEFT}}}};
+
+inputs::Status inputs::InputHandler::_updateKeys(KeyboardKey key)
+{
+    inputs::Status keyStatus = this->_keysHandler[key];
+
+    if (IsKeyDown(key)) {
+        return inputs::Status::PRESSED;
+    } else {
+        if (keyStatus == inputs::Status::PRESSED)
+            return inputs::Status::RELEASED;
+    }
+    return inputs::Status::DEFAULT;
+}
+
+void inputs::InputHandler::getActions(std::vector<common::Action>& actions)
+{
+    for (const auto& [key, command] : commands) {
+        inputs::Status newKeyStatus = this->_updateKeys(key);
+
+        this->_keysHandler[key] = newKeyStatus;
+        const auto it = command.find(newKeyStatus);
+
+        if (it != command.end())
+            actions.push_back(it->second);
+    }
+}
--- src/inputs/InputHandler.hpp
+++ src/inputs/InputHandler.hpp
@@ -2,20 +2,16 @@
 
 #include <unordered_map>
 #include <vector>
-#include "types/types.hpp"
 #include "raylib.h"
+#include "types/types.hpp"
 
 namespace inputs {
 
-    enum class Status {
-        DEFAULT,
-        PRESSED,
-        RELEASED
-    };
+    enum class Status { DEFAULT, PRESSED, RELEASED };
 
     struct Key {
-        KeyboardKey key;
-        Status status;
+            KeyboardKey key;
+            Status status;
     };
 
     class InputHandler {
@@ -24,11 +20,12 @@ namespace inputs {
             ~InputHandler() = default;
 
             // getActions detects inputs and adds actions in list given in parameters.
-            void getActions(std::vector<common::Action> &actions);
+            void getActions(std::vector<common::Action>& actions);
+
         private:
             // updateKeys checks keyboards input status and updates _keysHandler.
             inputs::Status _updateKeys(KeyboardKey key);
 
             std::unordered_map<KeyboardKey, inputs::Status> _keysHandler;
     };
-}
+} // namespace inputs
