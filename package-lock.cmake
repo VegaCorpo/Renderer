33de766643dce694180313ddb7a27eb77cf552aa
baptistebd <baptiste.blambert@epitech.eu>
2026-08-06 01:40:59 +0200

    feat: remove ui from render

--- package-lock.cmake
+++ package-lock.cmake
@@ -33,12 +33,3 @@ CPMDeclarePackage(raylib_cpp
     SYSTEM YES
     EXCLUDE_FROM_ALL YES
 )
-
-# Dear Imgui
-CPMDeclarePackage(ImGui
-    GIT_TAG v1.92.6
-    GITHUB_REPOSITORY ocornut/imgui
-    DOWNLOAD_ONLY YES
-    SYSTEM YES
-    EXCLUDE_FROM_ALL YES
-)
